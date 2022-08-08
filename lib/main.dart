bb0870c4226cbac3905266a280ec8e5f3aebcfd6
Omda090 <mohamedbushara.169@outlook.com>
2022-08-08 14:59:38 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:chatapp/screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'screens/welcome_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MessageMe app ',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: RegistrationScreen(),
+    );
+  }
+}
